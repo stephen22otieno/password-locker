43218afb9dbd73d6fecbaf082cd647419c84e982
stephen22otieno <stephenombiro22@gmail.com>
2018-11-02 11:57:01 +0300

    password-locker.py

--- password-locker.py
+++ password-locker.py
@@ -3,4 +3,5 @@ class Password
 Class that generates new instances of password
 """
 
-def __init__(self,first_name,last_name,log_in,password,sing_up)
+def __init__(self,first_name,last_name,log_in,password,sing_up):
+    
