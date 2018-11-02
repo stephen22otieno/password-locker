1556235215e7dac911a0dbebf9c4e85f40191b0c
stephen22otieno <stephenombiro22@gmail.com>
2018-11-02 13:14:07 +0300

    password_test.py

---
+++ password_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from password_locker import Password # Importing the password class
+
+class TestPassword(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the password class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
