5aa4d08da83b3413248381c26415bc1bd4657b65
stephen22otieno <stephenombiro22@gmail.com>
2018-11-02 12:43:02 +0300

    password-locker.py

---
+++ password_locker.py
@@ -0,0 +1,16 @@
+class Passsword
+"""
+Class that generates new instances of password
+"""
+
+     password_list = [] # Empty passsword list
+
+def __init__(self,first_name,last_name,login,password,sign_up):
+
+        # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login = login
+        self.password = password
+        self.sign-up = sign-up  
