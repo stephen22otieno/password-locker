b60255ca7952e0acde06244d253320e533a738e1
stephen22otieno <stephenombiro22@gmail.com>
2018-11-02 14:57:49 +0300

    password_test.py

--- password_test.py
+++ password_test.py
@@ -10,4 +10,23 @@ class TestPassword(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
-    
+    #Items up here ........
+
+    def setUp(self):
+        '''
+        set up to run before each test cases.
+        '''
+        self.new_password = Password("Stephen","Otieno","Login","1234456789") #create password objects
+
+        def test_init(self):
+          '''
+          test_init test case to test if the object is initialized properly
+          '''
+
+          self.assertEqual(self.new_password.first_name,"Stephen")
+          self.assertEqual(self.new_password.last_name,"Otieno")
+          self.assertEqual(self.new_password.login,"Login")
+          self.assertEqual(self.new_password.password,"123456789")
+
+        if __name__ == '__main__':
+            unittest.main()
