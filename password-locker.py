55f25d8b785183e568adf4cd414f84a283cfada3
stephen22otieno <stephenombiro22@gmail.com>
2018-11-02 12:08:21 +0300

    password-locker.py

--- password-locker.py
+++ password-locker.py
@@ -3,9 +3,15 @@ class Password
 Class that generates new instances of password
 """
 
-def __init__(self,first_name,last_name,log_in,password,sing_up):
+def __init__(self,first_name,last_name,login,password,sing_up):
 
     '''
       __init__method that helps us define properties for our objects.
       '''
-      
+
+        Args: 
+           first_name: New password-locker first name.
+           last_name: New password-locker last name.
+           login:New password-locker login.
+           password:New password-locker password.
+           sing up:New password-locker sing-up.
