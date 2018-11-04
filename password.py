86bb33474b10fb0cb505219d202f0dacf9b8b64b
stephen22otieno <stephenombiro22@gmail.com>
2018-11-04 11:55:03 +0300

    password.py

--- password.py
+++ password.py
@@ -1,16 +1,15 @@
- class Password:
+class Password:
     """
-    Class that generates new instances of passwords
+    Class that generates new instances of passwords.
     """
 
     password_list = [] # Empty password list
 
-    def __init__(self,first_name,last_name,phone_number,email,gander):
+    def __init__(self,first_name,last_name,number,email):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
-        self.email = email  
-        self.gander = gander
+        self.email = email
