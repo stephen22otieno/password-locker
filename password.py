4380b134ea8db0cc18dd8e238d9849794118358c
stephen22otieno <stephenombiro22@gmail.com>
2018-11-04 11:30:47 +0300

    password.py

--- password.py
+++ password.py
@@ -5,11 +5,12 @@
 
     password_list = [] # Empty password list
 
-    def __init__(self,first_name,last_name,phone_number,email):
+    def __init__(self,first_name,last_name,phone_number,email,gander):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
-        self.email = email
+        self.email = email  
+        self.gander = gander
