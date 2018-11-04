dd64e0642d270baa81345976172a182dd8b28d6f
stephen22otieno <stephenombiro22@gmail.com>
2018-11-04 11:24:31 +0300

    password.py

--- password.py
+++ password.py
@@ -3,4 +3,11 @@
     Class that generates new instances of passwords
     """
 
-    pass
+    def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
